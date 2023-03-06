6714552e534db5346e301855198a6b2d9dab835d
MS <108641119+maybeslow@users.noreply.github.com>
2023-03-06 11:19:06 +0300

    Add files via upload

---
+++ ALL.py
@@ -0,0 +1,5 @@
+
+
+
+CHOICE_LANG = "🇹🇷 Lütfen şimdi dilinizi seçin.\n\n🇬🇧 Please select your language.\n\n🇦🇿 İndi dilinizi seçin.\n\n🇷🇺 Пожалуйста, выберите язык сейчас."
+LANGAUGE = "🏳️‍🌈 Language"
